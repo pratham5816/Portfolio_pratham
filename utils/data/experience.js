b38a0fc6b961d93a29f0b201224f5c0d3d90f3ba
pratham5816 <163821803+pratham5816@users.noreply.github.com>
2025-04-20 19:17:50 +0530

    Update experience.js

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -3,13 +3,13 @@ export const experiences = [
     id: 1,
     title: 'Final-Year Computer Science Student',
     company: "Gyan Ganga College Of Technology",
-    duration: "(October 2021 - June 2025)"
+    duration: "October 2021 - June 2025"
   },
   {
     id: 2,
     title: "Completed the Wipro TalentNext Program (JAVA)",
     company: "Wipro Project Based Learning Application",
-    duration: "(May 2024 - September 2024)"
+    duration: "May 2024 - September 2024"
   },
   // {
   //   id: 3,
