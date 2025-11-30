89a3b9fe3a6c4070a96580fc478b905ebafda81b
pratham5816 <163821803+pratham5816@users.noreply.github.com>
2025-11-30 19:14:56 +0530

    Update education entries and fix IDs

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,14 +1,22 @@
 export const educations = [
   {
     id: 1,
+    title: "",
+    duration: "2025 - 2026",
+    institution: "Centre for Development of Advanced Computing (CDAC).",
+    score: "",
+    program: "PG Certificate Programme in Advanced Computing (PGCP-AC) / (PG-DAC)",
+  },
+  {
+    id: 2,
     title: "Bachelor Degree",
-    duration: "2021 - 2025 Present",
+    duration: "2021 - 2025",
     institution: "Gyan Ganga College Of Technology , Jabalpur",
     score: "Academic Performance - 7.0 CGPA",
     program: "Program - Computer Science Engineering",
   },
   {
-    id: 2,
+    id: 3,
     title: "Higher Secondary Certificate",
     duration: "2020 - 2021",
     institution: "Yash Nursery H S School , Jabalpur",
@@ -16,7 +24,7 @@ export const educations = [
     program: "Program - PCM",
   },
   {
-    id: 3,
+    id: 4,
     title: "Secondary School Certificate",
     duration: "2018 - 2019",
     institution: "Kendriya Vidyalaya VFJ , Jabalpur",
