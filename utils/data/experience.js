00ef5de1ba12ae07a5723e5f5ca1754a26ea0823
pratham5816 <163821803+pratham5816@users.noreply.github.com>
2025-11-30 19:15:18 +0530

    Update experiences with new entry and ID changes
    Added a new experience entry and updated IDs.

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,20 @@
 export const experiences = [
   {
     id: 1,
+    title: 'PG Certificate Programme in Advanced Computing (PGCP-AC) / (PG-DAC)',
+    company: "Centre for Development of Advanced Computing (CDAC).",
+    duration: "August 2025 -  February 2026"
+  },
+  {
+    id: 2,
     title: 'Final-Year Computer Science Student',
     company: "Gyan Ganga College Of Technology",
     duration: "October 2021 - June 2025"
   },
   {
-    id: 2,
+    id: 3,
     title: "Completed the Wipro TalentNext Program (JAVA)",
     company: "Wipro Project Based Learning Application",
     duration: "May 2024 - September 2024"
-  },
-  // {
-  //   id: 3,
-  //   title: "Self Employed",
-  //   company: "Code and build something in everyday.",
-  //   duration: "(Jan 2018 - Present)"
-  // }
+  }
 ]
